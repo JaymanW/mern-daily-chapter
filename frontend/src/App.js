5be5e5aab35af4a2e74f21a1c0db915909832976
Jayden W <jaymanw2000@gmail.com>
2021-09-16 18:32:23 -0500

    passage functionality and connection to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+
+import moment from 'moment';
+
 import Header from './components/Header'
+import Passage from './components/Passage/index'
 
 function App() {
+  const [offset, setOffset] = useState(0);
+  const [date, setDate] = useState(moment().format("MM-DD-YYYY"))
+  
+  useEffect(() => {
+      const newDate = moment().subtract(offset, 'days').format("MM-DD-YYYY");
+      setDate(newDate)
+  }, [offset])
+  
+  const handleBackOneDay = () => {
+      setOffset(offset => offset + 1);
+  }
+
+  const handleForwardOneDay = () => {
+      if (offset > 0) {
+          setOffset(offset => offset - 1);
+      }
+  }
+  
   return (
     <div className="App">
-      <Header />
-      {/* <Selectors /> */}
-      {/* <Passage /> */}
+      <Header onBackOneDay={handleBackOneDay} onForwardOneDay={handleForwardOneDay} date={date} />
+      <Passage date={date} />
       {/* <Comments /> */}
     </div>
   );
